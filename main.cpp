3f0772553e26c13383c754051fb5e4f8b7548f76
Alexander Potashev <aspotashev@gmail.com>
2009-12-19 23:45:33 +0300

    Read "-t [nsecs]" from command line

--- main.cpp
+++ main.cpp
@@ -5,7 +5,22 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	MainWindow mw(3);
+	int opt;
+	int nsecs = 300;
+	while ((opt = getopt(argc, argv, "t:")) != -1)
+	{
+		switch (opt)
+		{
+		case 't':
+			nsecs = atoi(optarg);
+			break;
+		default:
+			fprintf(stderr, "Usage: guess!\n");
+			exit(1);
+		}
+	}
+
+	MainWindow mw(nsecs);
 	mw.showFullScreen();
 
 	return app.exec();
